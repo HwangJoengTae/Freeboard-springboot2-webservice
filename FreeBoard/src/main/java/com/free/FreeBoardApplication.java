a1049fcd6eb09b98fc5bd2af00c38a882f3c1882
Jeong <soin20222022@gmail.com>
2022-11-07 13:46:43 +0900

    톰캣설정

--- FreeBoard/src/main/java/com/free/FreeBoardApplication.java
+++ FreeBoard/src/main/java/com/free/FreeBoardApplication.java
@@ -3,10 +3,17 @@ package com.free;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FreeBoardApplication {
-
+public class FreeBoardApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FreeBoardApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FreeBoardApplication.class, args);
 	}
