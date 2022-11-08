a2888eff97bd5ef912d69412e3f0f56771eaccf7
Jeong <soin20222022@gmail.com>
2022-11-09 00:42:36 +0900

    내장으로 변경

--- FreeBoard/src/main/java/com/free/FreeBoardApplication.java
+++ FreeBoard/src/main/java/com/free/FreeBoardApplication.java
@@ -7,12 +7,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FreeBoardApplication extends SpringBootServletInitializer {
+public class FreeBoardApplication  {
 	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(FreeBoardApplication.class);
-    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FreeBoardApplication.class, args);
