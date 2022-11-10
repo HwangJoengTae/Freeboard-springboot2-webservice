64e541df9545d5725f671df7b3e3c455c3561580
Jeong <soin20222022@gmail.com>
2022-11-10 23:31:03 +0900

    다시 실행

--- FreeBoard/deploy-before.sh
+++ FreeBoard/deploy-before.sh
@@ -1,3 +1,4 @@
-sudo rm -rf /home/ec2-user/build/
-
-sudo mkdir  /home/ec2-user/build/
+if [ -d /home/ec2-user/build ]; then
+    sudo rm -rf /home/ec2-user/build/
+fi
+sudo mkdir -vp /home/ec2-user/build/
