d2865a4b24d3b1829b91b2d09f0291b1cdf53924
Jeong <soin20222022@gmail.com>
2022-11-10 23:04:09 +0900

    경로 변경

--- FreeBoard/deploy.sh
+++ FreeBoard/deploy.sh
@@ -1,3 +1,3 @@
 cd ~/build
 
-java -jar FreeBoard-0.0.1-SNAPSHOT.war --spring.config.location=C:/apps/application.properties,C:/apps/application-oauth.properties
+java -jar FreeBoard-0.0.1-SNAPSHOT.war --spring.config.location=/home/ec2-user/app/application.properties,/home/ec2-user/app/apps/application-oauth.properties
